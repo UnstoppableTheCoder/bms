4b9740d23c185981b11c6dab92f8b96d342564e0
Vipendra Singh Patel <122035117+UnstoppableTheCoder@users.noreply.github.com>
2025-11-23 21:16:30 +0530

    Update user not found message for clarity

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { prisma } from "@repo/prisma/client";
 export default async function Home() {
   const user = await prisma.user.findFirst();
 
-  if (!user) return <div>No User Found</div>;
+  if (!user) return <div>No User Found. Sorry......!</div>;
 
   return (
     <div>
